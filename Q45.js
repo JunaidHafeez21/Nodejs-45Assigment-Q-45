09d3a9ba566e64687fda2c8d911d0296181e36a8
JunaidHafeez21 <160257992+JunaidHafeez21@users.noreply.github.com>
2024-02-19 17:20:07 +0500

    Add files via upload

---
+++ Q45.js
@@ -0,0 +1,11 @@
+"use strict";
+// Q45: Cars: Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.
+function createCar(manufacturer, model, ...options) {
+    const car = { manufacturer, model };
+    options.forEach(([key, value]) => {
+        car[key] = value;
+    });
+    return car;
+}
+const myCar = createCar('Honda', 'Civic', ['color', 'white'], ['year', '2022']);
+console.log(myCar);
---
+++ Q45.ts
@@ -0,0 +1,21 @@
+// Q45: Cars: Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.
+
+interface Car {
+    manufacturer: string;
+    model: string;
+    [key: string]: string; 
+}
+  function createCar(manufacturer: string, model: string, ...options: [string, string][]): Car {
+    const car: Car = { manufacturer, model };
+  
+    options.forEach(([key, value]) => {
+      car[key] = value;
+    });
+  
+    return car;
+  }
+  
+  const myCar = createCar('Honda', 'Civic', ['color', 'white'], ['year', '2022']);
+  
+  console.log(myCar);
+  
